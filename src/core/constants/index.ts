969e5a771630e86919bbd8d67453f2bc2add8631
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-19 17:49:28 -0500

    feat(core-constants): Add initial values

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './date.constant';
+export * from './initalValues.constant';
 export * from './regex.constant';
 export * from './requestErrors.constants';
---
+++ src/core/constants/initalValues.constant.ts
@@ -0,0 +1,6 @@
+import {IErrorType} from '@src/common/presenter/models';
+
+export const INITIAL_STATE_ERROR: IErrorType = {
+  isVisible: false,
+  label: '',
+};
