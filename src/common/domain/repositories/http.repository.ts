22919cb4c56efb13517ce881e87ac112e7e30922
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-18 20:02:25 -0500

    feat(common-domain): add repositories

---
+++ src/common/domain/repositories/http.repository.ts
@@ -0,0 +1,13 @@
+import {Method} from 'axios';
+
+export interface IRequestOption {
+  readonly method: Method;
+  readonly url: string;
+  readonly headers?: any;
+  readonly body?: {[key: string]: any};
+  readonly params?: {[key: string]: any};
+}
+
+export interface IHttp {
+  request<T>(requestOption: IRequestOption): Promise<T>;
+}
