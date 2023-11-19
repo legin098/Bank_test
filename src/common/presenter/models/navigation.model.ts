85728a7f705447c294b5d7b176d75dae3862f28e
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-19 17:48:44 -0500

    feat(common-models): path Add product

--- src/common/presenter/models/navigation.model.ts
+++ src/common/presenter/models/navigation.model.ts
@@ -3,6 +3,7 @@ import {ProductEntity} from '@src/products/domain/models';
 export type ProductStackParamList = {
   Home?: undefined;
   Detail?: ProductEntity;
+  Add?: undefined;
 };
 
 export type MainStackParamList = {
