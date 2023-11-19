05772594efbd82b7a392538c8c6221a0b36b3da3
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-19 06:36:24 -0500

    feat(products-infrastructure): add product adapter

---
+++ src/products/infrastructure/adapters/product.adapter.ts
@@ -0,0 +1,14 @@
+import {ProductEntity} from '@src/products/domain/models';
+import {ProductDto} from '../models';
+
+export class ProductAdapter {
+  public static ProductsDtoToEntity = (data: ProductDto[]): ProductEntity[] =>
+    data.map(product => ({
+      id: product.id,
+      name: product.name ?? '',
+      picture: product.logo ?? '',
+      description: product.description ?? '',
+      dateRelease: product.date_release ?? '',
+      dateRevision: product.date_revision ?? '',
+    }));
+}
