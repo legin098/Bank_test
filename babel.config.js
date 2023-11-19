c9253f0215108eae3bd1e97ebaaf63d37547825c
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-18 20:11:25 -0500

    chore: config paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@src': './src',
+          '@common': './src/common',
+          '@products': './src/products',
+        },
+      },
+    ],
+  ],
 };
