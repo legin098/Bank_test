6adf384c079b5be8af5cd5b7bfa4bf42b2aea005
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-19 06:33:00 -0500

    chore: Add path core

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
         alias: {
           '@src': './src',
           '@common': './src/common',
+          '@core': './src/core',
           '@products': './src/products',
         },
       },
