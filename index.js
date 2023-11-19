8f19962355946b71094d24a63308d354de169cfd
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-19 17:45:14 -0500

    chore: add libreries

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
 import App from '@src/App';
 import {name as appName} from './app.json';
+import '@formatjs/intl-locale/polyfill';
+import '@formatjs/intl-datetimeformat/polyfill';
+import '@formatjs/intl-datetimeformat/add-all-tz';
+import '@formatjs/intl-datetimeformat/locale-data/en';
+import '@formatjs/intl-datetimeformat/locale-data/es-CO';
 
 AppRegistry.registerComponent(appName, () => App);
