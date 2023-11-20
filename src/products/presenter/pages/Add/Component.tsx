f6292bf376d16ce5982b08853e6bed9b02393b0a
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-20 06:56:09 -0500

    feat(products-add): Add form product

--- src/products/presenter/pages/Add/Component.tsx
+++ src/products/presenter/pages/Add/Component.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Text, View} from 'react-native';
-import {Header} from '@src/common/presenter/components';
+import {
+  Button,
+  Header,
+  InputDate,
+  InputText,
+} from '@src/common/presenter/components';
 import {PropsComponent as Props} from './models';
 import {styles} from './styles';
+import {Controller, useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {ProductEntity} from '@src/products/domain/models';
+import {formProductSchemeValidation as schemeValidation} from '../../validations';
+import {PRODUCT_INITIAL_VALUES} from '../../constants';
+import {addOneYearToDate, formatDate} from '@src/core/utils';
 
 const Component = ({onNavigation}: Props) => {
+  const [dateRelease, setDateRelease] = useState<string>('');
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: {isValid, errors},
+  } = useForm<ProductEntity>({
+    resolver: yupResolver(schemeValidation) as any,
+    mode: 'onChange',
+    defaultValues: PRODUCT_INITIAL_VALUES,
+  });
+
   return (
     <View style={styles.container}>
       <Header />
@@ -12,9 +36,119 @@ const Component = ({onNavigation}: Props) => {
         bounces={false}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.ctnList}>
-        <View>
-          <Text>Formulario de Registro</Text>
-        </View>
+        <Text style={styles.title}>Formulario de Registro</Text>
+        <Controller
+          control={control}
+          render={({field: {onChange, value}}) => (
+            <InputText
+              containerStyles={styles.ctnInput}
+              value={value}
+              label={'ID'}
+              onChangeText={onChange}
+              {...(!!errors.id?.message && {
+                error: {
+                  isVisible: true,
+                  label: errors.id?.message,
+                },
+              })}
+            />
+          )}
+          name="id"
+          rules={{required: true}}
+        />
+        <Controller
+          control={control}
+          render={({field: {onChange, value}}) => (
+            <InputText
+              containerStyles={styles.ctnInput}
+              value={value}
+              label={'Nombre'}
+              onChangeText={onChange}
+              {...(!!errors.name?.message && {
+                error: {
+                  isVisible: true,
+                  label: errors.name?.message,
+                },
+              })}
+            />
+          )}
+          name="name"
+          rules={{required: true}}
+        />
+        <Controller
+          control={control}
+          render={({field: {onChange, value}}) => (
+            <InputText
+              containerStyles={styles.ctnInput}
+              value={value}
+              label={'Descripción'}
+              onChangeText={onChange}
+              {...(!!errors.description?.message && {
+                error: {
+                  isVisible: true,
+                  label: errors.description?.message,
+                },
+              })}
+            />
+          )}
+          name="description"
+          rules={{required: true}}
+        />
+        <Controller
+          control={control}
+          render={({field: {onChange, value}}) => (
+            <InputText
+              containerStyles={styles.ctnInput}
+              value={value}
+              label={'Logo'}
+              onChangeText={onChange}
+              {...(!!errors.picture?.message && {
+                error: {
+                  isVisible: true,
+                  label: errors.picture?.message,
+                },
+              })}
+            />
+          )}
+          name="picture"
+          rules={{required: true}}
+        />
+        <Controller
+          control={control}
+          render={({field: {onChange, onBlur, value}}) => (
+            <InputDate
+              containerStyles={styles.ctnInput}
+              value={!!value ? formatDate(value, "dd'/'LL'/'yyyy") : ''}
+              label={'Fecha Liberación'}
+              onChange={value => {
+                setDateRelease(value);
+                onChange(value);
+              }}
+              {...(errors.dateRelease?.message && {
+                error: {
+                  isVisible: true,
+                  label: errors.dateRelease?.message,
+                },
+              })}
+            />
+          )}
+          name="dateRelease"
+          rules={{required: true}}
+        />
+        <InputText
+          containerStyles={styles.ctnInput}
+          inputStyles={styles.disableInput}
+          value={addOneYearToDate(dateRelease)}
+          editable={false}
+          label={'Fecha Revisión'}
+        />
+
+        <Button text="Enviar" containerStyles={styles.btnSend} />
+        <Button
+          text="Reiniciar"
+          type="gray_light"
+          containerStyles={styles.btnReset}
+        />
       </ScrollView>
     </View>
   );
--- src/products/presenter/pages/Add/styles.ts
+++ src/products/presenter/pages/Add/styles.ts
@@ -13,10 +13,28 @@ export const styles = StyleSheet.create({
     flexGrow: 1,
     width,
     marginTop: width * 0.04,
-    paddingTop: width * 0.1,
+    paddingTop: width * 0.035,
     paddingHorizontal: '6%',
     paddingBottom: height * 0.03,
     borderTopWidth: 1,
     borderTopColor: COLORS.common.gray_light,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: width * 0.05,
+  },
+  ctnInput: {
+    marginBottom: width * 0.027,
+  },
+  disableInput: {
+    opacity: 0.3,
+    backgroundColor: COLORS.common.gray_light,
+  },
+  btnSend: {
+    marginTop: width * 0.05,
+  },
+  btnReset: {
+    marginVertical: width * 0.025,
   },
 });
