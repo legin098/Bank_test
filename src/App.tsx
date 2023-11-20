a3a7a3e695a9351918a230509f594acdcb38027f
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-20 15:21:25 -0500

    chore(app): remove warning animated

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigation from '@common/presenter/navigation';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {LogBox} from 'react-native';
 
 const queryClient = new QueryClient();
 
 const App = () => {
+  LogBox.ignoreLogs([
+    'Sending `onAnimatedValueUpdate` with no listeners registered', // TODO: Remove when fixed
+  ]);
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
