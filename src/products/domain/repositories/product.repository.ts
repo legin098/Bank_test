afd113c5ac038e7bbda0878c00e8fd8bf12159d6
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-20 15:22:59 -0500

    feat(products-domain): addProduct and verificationProduct

--- src/products/domain/repositories/product.repository.ts
+++ src/products/domain/repositories/product.repository.ts
@@ -2,4 +2,6 @@ import {ProductEntity} from '../models';
 
 export interface IProductRepository {
   getProducts(): Promise<ProductEntity[]>;
+  addProduct(product: ProductEntity): Promise<void>;
+  verificationProduct(productID: string): Promise<boolean>;
 }
--- src/products/domain/useCases/product.useCase.ts
+++ src/products/domain/useCases/product.useCase.ts
@@ -6,4 +6,10 @@ export class ProductUseCase {
 
   getProducts = async (): Promise<ProductEntity[]> =>
     await this.repository.getProducts();
+
+  addProduct = async (data: ProductEntity): Promise<void> =>
+    await this.repository.addProduct(data);
+
+  verificationProduct = async (productID: string): Promise<boolean> =>
+    await this.repository.verificationProduct(productID);
 }
