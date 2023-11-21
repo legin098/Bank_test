ca1340f3b4297d2845b08af223562296585349be
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2023-11-21 10:01:44 -0500

    chore: Adjust test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: [
-    './node_modules/react-native-gesture-handler/jestSetup.js',
-    './__tests__/__mocks__/react-native.js',
-    './__tests__/__mocks__/react-navigation.js',
-  ],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   transformIgnorePatterns: [
     'jest-runner',
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
